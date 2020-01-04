b61a1be4e3c68d4fd93d8039944fa089c1de1a12
Osiński Paweł <pawel.osinski@pentacomp.pl>
2020-01-04 21:17:57 +0100

    hibernate

---
+++ api/initial/src/test/java/bank/ApplicationTest.java
@@ -0,0 +1,12 @@
+package bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ApplicationTest {
+    @Test
+    public void testDemo() throws Exception {
+        assertTrue(true);
+    }
+}
